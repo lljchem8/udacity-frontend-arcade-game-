19da44fe10261406ea7babfa8f213058284656c8
Liji Le <lijile@Lijis-MacBook-Pro.local>
2019-03-09 01:47:11 -0500

    first version of the arcade game

--- js/app.js
+++ js/app.js
@@ -1,37 +1,65 @@
-// Enemies our player must avoid
-function generateRandomSpeed() {
-  return Math.floor((Math.random() * 120) + 50);
+
+function generateRandomSpeed(level) {
+  return Math.floor((Math.random() * 80) + 80)*(1+level/10);
 }
 
+function generateInitialPosition() {
+  return Math.floor(Math.random()*400);
+}
 
+function addEnemies (allEnemies) {
+  const levelElement = document.querySelector(".levelNum");
+  const levelNum = parseInt(levelElement.innerText);
 
+  allEnemies.push(new Enemy(generateInitialPosition(), 60,generateRandomSpeed(levelNum)));
+  allEnemies.push(new Enemy(generateInitialPosition(), 143,generateRandomSpeed(levelNum)));
+  allEnemies.push(new Enemy(generateInitialPosition(),143+83,generateRandomSpeed(levelNum)));
+  console.log(levelNum);
+}
 
+function restartGame() {
+  player.x = 202;
+  player.y = 392;
+  allEnemies = [];
+  level = 1;
+  addEnemies(allEnemies);
+}
 
-// Now instantiate your objects.
+// instantiate objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 const player = new Player(202,392);
-const allEnemies = [];
-(function addEnemies () {
-  allEnemies.push(new Enemy(-1, 60,generateRandomSpeed()));
-  allEnemies.push(new Enemy(-1, 143,generateRandomSpeed()));
-  allEnemies.push(new Enemy(-1,143+83,generateRandomSpeed()));
-}());
+let allEnemies = [];
+addEnemies(allEnemies);
+let level = 1;
+
 
 function checkCollisions() {
-  allEnemies.forEach(function(enimy) {
-    if (enimy.collision(player)) {
-      player.x = 202;
-      player.y = 392;
-      alert("game over");
+  allEnemies.forEach(function(enemy) {
+    if (enemy.collision(player)) {
+      const modalElement = document.querySelector(".modal");
+      modalElement.style.display = "block";
     }
   })
 }
 
+// hide modal and restart the game
+document.addEventListener('click', function(event) {
+    console.log(event.target.classList);
+    const modalElement = document.querySelector(".modal");
 
+    // if user click close or play again, the game restart
+    if (event.target.id == "play-again" || event.target.classList[0] == "close") {
+      modalElement.style.display = "none";
+      restartGame();
+    }
 
-
-
+    // if the user clicks anywhere outside of the modal, close it
+    if (event.target == modalElement) {
+      modalElement.style.display = "none";
+      restartGame();
+    }
+})
 
 
 // This listens for key presses and sends the keys to your
